f6442417677082b0e565fecaaf489084f9ca5072
Dan Pasanen <dan.pasanen@gmail.com>
2013-05-01 18:16:10 -0500

    msm8960: DeviceSettings: add control for panel color temp
    Change-Id: I7e7a4298ceb8d18baba989f8a3b3260678831791

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -45,6 +45,7 @@ public class DeviceSettings extends FragmentActivity {
     public static final String KEY_TOUCHSCREEN_SENSITIVITY = "touchscreen_sensitivity";
     public static final String KEY_TOUCHKEY_LIGHT = "touchkey_light";
     public static final String KEY_TOUCHKEY_BLN = "touchkey_bln";
+    public static final String KEY_PANEL_COLOR_TEMPERATURE = "panel_color_temperature";
     public static final String KEY_CABC = "cabc";
 
     public static final String CATEGORY_MDNIE = "mdnie";
