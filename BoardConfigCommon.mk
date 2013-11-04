0d195c0eba8bbe40bf4ebe8e92160da12a763034
bmc08gt <brandon.mcansh@gmail.com>
2013-11-04 23:09:24 +0100

    msm8960: neon-vfpv4 -> neon
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,8 +31,8 @@ ifneq ($(VARIENT_REQUIRE_3.0_KERNEL),true)
 COMMON_GLOBAL_CFLAGS += -DNEW_ION_API
 endif
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_CPU_VARIANT := krait
 
 # Adreno configuration
